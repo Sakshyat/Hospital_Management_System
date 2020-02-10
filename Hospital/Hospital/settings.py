9b34ca0ff8f75f3d2488ed13e61726ceff7a0db5
Sakshyat <sakshyats@gmail.com>
2020-02-10 10:14:02 +0545

    Start Project Update

--- Hospital/Hospital/settings.py
+++ Hospital/Hospital/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'doctor.apps.DoctorConfig',
-    'patient.apps.PatientConfig',
+    
+    
 ]
 
 MIDDLEWARE = [
