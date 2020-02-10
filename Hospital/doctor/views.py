8228c2414fcf9fe8d383abaeb3e2922c9d858c75
ven-ess <np03a180049@heraldcollege.edu.np>
2020-02-10 10:19:07 +0545

    Doctor views

--- Hospital/doctor/views.py
+++ Hospital/doctor/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from .models import Doctor
+from django.db.models import Q
+
 
 # Create your views here.
+
+def index(request):
+    return render(request=request, template_name="doctor/index.html", context={})
+
+
+def list_doc(request):
+    query = " "
+    context = {}
+    if request.GET:
+        query = request.GET['q']    
+    
+    
+    doctor = get_data_queryset(query)
+    context['doctors'] = doctor
+    return render(request, "doctor/search_doctor.html", context)
+
+
+def get_data_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        doctors = Doctor.objects.filter(Q( doc_name__icontains=q) | Q(doc_email__icontains=q) | Q(specialisation__icontains=q))
+        
+        for doctor in doctors:
+            queryset.append(doctor)
+    
+    return list(set(queryset)) 
